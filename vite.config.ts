a30fb68492a0c83f59d93a4aa0128f4ddf2cf028
UDAYAKUMAR D <122508378+Udaya1011@users.noreply.github.com>
2025-12-25 08:40:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,23 @@ import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
   server: {
-    host: true,          // allow LAN access
+    host: true,
     port: 5173,
+    allowedHosts: [
+      "air-ambulance-frontend-1.onrender.com"
+    ],
     proxy: {
       "/api": {
-        target: "http://localhost:8000", // ✅ BACKEND
+        target: "http://localhost:8000",
         changeOrigin: true,
         secure: false,
       },
     },
+  },
+  preview: {
+    allowedHosts: [
+      "air-ambulance-frontend-1.onrender.com"
+    ]
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
